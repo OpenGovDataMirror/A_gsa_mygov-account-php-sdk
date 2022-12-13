68e2e0c35820c754a4911f5f31afda8bf0c87213
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-14 01:59:03 +0200

    init

---
+++ application/views/docs.php
@@ -0,0 +1,6 @@
+<?php include 'include/header-meta.php'; ?>
+<?php include 'include/header.php';?>
+
+<h2> Docs and overview </h2>
+
+<?php include 'include/footer.php';?>
---
+++ application/views/include/header-meta.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+<title>MyGov App Example</title>
+
+<?php if(strlen(config_item('app_url')) > 0): ?>
+<base href="<?php echo config_item('app_url')?>" />
+<?php endif;?>
+
+
+<!-- Le styles -->
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
+
+<link type="text/css" href="./assets/css/style.css" rel="stylesheet">
+
+<link media="print" type="text/css" href="./assets/css/print.css" rel="stylesheet">
+
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- Bootstrap CSS fixes for IE6 -->
+<!--[if lt IE 7]><link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap-ie6.min.css"><![endif]-->
+
+  <!-- Le fav and touch icons 
+  <link rel="shortcut icon" href="/assets/ico/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./assets/ico/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./assets/ico/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./assets/ico/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="./assets/ico/apple-touch-icon-57-precomposed.png">
+  -->
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script>	
+		
+
+
+  
